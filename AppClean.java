1f4a47eab0286f7fa6d3bb94fd58dd32d731eabe
DavidShv-git <73021956+DavidShv-git@users.noreply.github.com>
2021-02-28 07:56:25 +0300

    Add files via upload

--- AppClean.java
+++ AppClean.java
@@ -4,7 +4,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static java.util.Arrays.*;
+
 public class AppClean {
+    private static int[] numbers;
 
     public static void  main(String[] args){
 
@@ -13,16 +16,49 @@ public class AppClean {
     }
 
     public static List<Integer> removeDoubleVolume(){
-        List<Integer> firstList = Arrays.asList(1,1,1,1,2,2,3,4,5);
+        int[] arr1 = {5,5,2,2,1,1,1,2,2,3,4,5,2,2};
+        numbers = arr1;
+        if(numbers.length == 0) return new ArrayList<Integer>();
+        quickSort(0, numbers.length-1);
+
         List<Integer> secondList = new ArrayList<>();
-        int temporary = 0;
-        for (Integer i : firstList) {
-            if (temporary != i){
-                temporary = i;
+        int temporary = numbers[0];
+        secondList.add(temporary);
+        for (int i=0; i < numbers.length; i++) {
+            if (temporary != numbers[i]){
+                temporary = numbers[i];
                 secondList.add(temporary);
             }
         }
         return secondList;
+    }
 
+    private static void quickSort(int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(begin, end);
+
+            quickSort(begin, partitionIndex-1);
+            quickSort(partitionIndex+1, end);
+        }
+    }
+
+    private static int partition(int begin, int end) {
+        int pivot = numbers[end];
+        int i = (begin-1);
+        for (int j = begin; j < end; j++) {
+            if (numbers[j] <= pivot) {
+                i++;
+
+                int swapTemp = numbers[i];
+                numbers[i] = numbers[j];
+                numbers[j] = swapTemp;
+            }
+        }
+
+        int swapTemp = numbers[i+1];
+        numbers[i+1] = numbers[end];
+        numbers[end] = swapTemp;
+
+        return i+1;
     }
 }
